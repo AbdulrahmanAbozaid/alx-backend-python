4496816b19befe6f2bd412f18df5576469203cb2
AbdulrahmanAbozaid <diver72003@gmail.com>
2024-11-25 16:30:45 +0200

    Unit test test_client2

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -6,16 +6,18 @@ from typing import Any
 import client
 from parameterized import parameterized
 from unittest import TestCase
-from unittest.mock import patch, Mock
+from unittest.mock import PropertyMock, patch, Mock
 
 
 class TestGithubOrgClient(TestCase):
     """Test Github Class"""
 
-    @parameterized.expand([
-        ('google', 42),
-        ('abc', 42),
-    ])
+    @parameterized.expand(
+        [
+            ("google", 42),
+            ("abc", 42),
+        ]
+    )
     @patch("client.get_json", new_callable=Mock)
     def test_org(self, org: str, expected: Any, mock_json: Mock) -> None:
         """Test the org func"""
@@ -23,3 +25,11 @@ class TestGithubOrgClient(TestCase):
         git_org = client.GithubOrgClient(org)
         self.assertEqual(git_org.org, expected)
         mock_json.assert_called_once_with(f"https://api.github.com/orgs/{org}")
+
+    def test_public_repos_url(self) -> None:
+        """Test public apis"""
+        with patch("client.GithubOrgClient.org",
+                   new_callable=PropertyMock) as mock_org:
+            mock_org.return_value = {"repos_url": 42}
+            git_org = client.GithubOrgClient("google")
+            self.assertEqual(git_org._public_repos_url, 42)
