ccedcff69937cc75f2dd7310434956b5da32e3ee
AbdulrahmanAbozaid <diver72003@gmail.com>
2024-11-25 03:34:27 +0200

    Unit test 1

--- 0x03-Unittests_and_integration_tests/main_0.py
+++ 0x03-Unittests_and_integration_tests/main_0.py
@@ -2,15 +2,33 @@
 """
 Main Module To Test
 """
-from utils import access_nested_map, get_json
+from utils import access_nested_map, get_json, memoize
 from unittest import TestCase
 from unittest.mock import Mock, patch
 from parameterized import parameterized
 from utils import access_nested_map, get_json
 
 
-with patch("__main__.get_json", new_callable=Mock) as mockJ:
-    mockJ.json.return_value = 'foo'
-    get_json.json('https://jsonplaceholder.typicode.com/posts/1')
-    get_json.json.assert_called_once_with('shttps://jsonplaceholder.typicode.com/posts/1')
+class TestMemoize(TestCase):
+    """Test Memoization decorator"""
 
+    def test_memoize(self):
+        """Test Memoize"""
+
+        class TestClass:
+
+            def a_method(self):
+                return 42
+
+            @memoize
+            def a_property(self):
+                return self.a_method()
+
+        with patch("test_utils.TestClass.a_method", new_callable=Mock) as mock_m:
+            mock_m.return_value = 42
+            inst = TestClass()
+            self.assertEqual(inst.a_property, 42)
+            self.assertEqual(inst.a_property, 42)
+            mock_m.assert_called_once()
+
+print(__name__)
--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -6,7 +6,7 @@ from typing import Any, Mapping, Sequence
 from unittest import TestCase
 from unittest.mock import Mock, patch
 from parameterized import parameterized
-from utils import access_nested_map
+from utils import access_nested_map, memoize
 import utils
 
 
@@ -56,3 +56,26 @@ class TestGetJson(TestCase):
         mock_api.json.return_value = test_payload
         self.assertEqual(utils.get_json.json(url), test_payload)
         mock_api.json.assert_called_once_with(url)
+
+
+class TestMemoize(TestCase):
+    """Test Memoization decorator"""
+
+    def test_memoize(self):
+        """Test Memoize"""
+
+        class TestClass:
+
+            def a_method(self):
+                return 42
+
+            @memoize
+            def a_property(self):
+                return self.a_method()
+
+        with patch.object(TestClass, "a_method") as mock_m:
+            mock_m.return_value = 42
+            inst = TestClass()
+            self.assertEqual(inst.a_property, 42)
+            self.assertEqual(inst.a_property, 42)
+            mock_m.assert_called_once()
